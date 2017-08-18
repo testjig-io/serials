856df2ae249d101851f7d1550b5120b7a0500441
Mario Ribeiro <mario@rasgo.xyz>
2017-08-18 11:39:42 +0100

    Update

--- src/serials.c
+++ src/serials.c
@@ -1,4 +1,5 @@
 #include "serials.h"
+#include <string.h>
 
 void _serials_pkt_init(serials_pkt_t *pkt);
 void _serials_pkt_send(serials_t *instance, serials_pkt_t *pkt);
@@ -6,19 +7,46 @@ void _serials_rxpkt_build(serials_t *instance);
 void _serials_send_byte(serials_t *instance, uint8_t data);
 void _serials_send_data(serials_t *instance, uint8_t *data, uint32_t count);
 
+#if SERIALS_ARG_TO_ASCII
+static void _ui8tohex(uint8_t n, char *buf)
+{
+//    char const digit[] = "0123456789";
+//    uint8_t ch_cnt = 0;
+//    char *b = buf;
+//    do
+//    {
+//        *b = digit[i%10];
+//        i = i/10;
+//        b++;
+//        ch_cnt++;
+//    }while(i > 0);
+//    *b = '\0';
+//    return ch_cnt;
+	buf[1] = (n & 0xF) + '0';
+	buf[0] = ((n >> 4) & 0xF) + '0';
+}
+#endif
+
 void serials_init(serials_t *instance, serials_init_t *init)
+{
+    instance->rxbuf = init->rxbuf;
+    instance->rxbuf_len = init->rxbuf_len;
+
+    instance->fn_send_byte = init->fn_send_byte;
+    instance->cb_rxpkt_ready = init->cb_rxpkt_ready;
+
+    serials_reset(instance);
+}
+
+void serials_reset(serials_t *instance)
 {
     instance->rxpkt_ready = false;
     instance->rx_cnt = 0;
     instance->bs_esc = false;
     instance->overflow = false;
+    instance->rxing = false;
 
-    _serials_pkt_init(&(instance->rx_pkt));
-    instance->rx_buf = init->rx_buf;
-    instance->rx_buf_len = init->rx_buf_len;
-
-    instance->fn_send_byte = init->fn_send_byte;
-    instance->cb_rxpkt_ready = init->cb_rxpkt_ready;
+    _serials_pkt_init(&(instance->rxpkt));
 }
 
 void serials_parse_bytes(serials_t *instance, uint8_t *data, uint32_t count)
@@ -27,6 +55,9 @@ void serials_parse_bytes(serials_t *instance, uint8_t *data, uint32_t count)
     for(i = 0; i < count; i++)
     {
         uint8_t b = data[i];
+#if SERIALS_ECHO_RX
+        instance->fn_send_byte(b);
+#endif
 
         if(b == SERIALS_BS_ESC && !instance->bs_esc)
         {
@@ -36,37 +67,41 @@ void serials_parse_bytes(serials_t *instance, uint8_t *data, uint32_t count)
 
         if(b == SERIALS_BS_BEGIN && !instance->bs_esc)
         {
-            _serials_pkt_init(&(instance->rx_pkt));
+            _serials_pkt_init(&(instance->rxpkt));
             instance->rx_cnt = 0;
             instance->rxpkt_ready = false;
+            instance->rxing = true;
             continue;
         }
 
         if(b == SERIALS_BS_END && !instance->bs_esc)
         {
             _serials_rxpkt_build(instance);
+            instance->rxing = false;
+            instance->rxpkt_ready = true;
             if(instance->cb_rxpkt_ready != 0)
             {
                 instance->cb_rxpkt_ready();
             }
-            else
-            {
-                instance->rxpkt_ready = true;
-            }
             continue;
         }
 
-        if(instance->rx_cnt < instance->rx_buf_len)
+        instance->bs_esc = false;
+
+        if(instance->rxing == false)
         {
-            instance->rx_buf[instance->rx_cnt] = b;
+        	continue;
+        }
+
+        if(instance->rx_cnt < (instance->rxbuf_len - 1))
+        {
+            instance->rxbuf[instance->rx_cnt] = b;
             instance->rx_cnt += 1;
         }
         else
         {
             instance->overflow = true;
         }
-
-        instance->bs_esc = false;
     }
 }
 
@@ -88,14 +123,15 @@ void serials_rxpkt_ready_clear(serials_t *instance)
 serials_pkt_t* serials_rxpkt(serials_t *instance)
 {
     if(instance->rxpkt_ready)
-        return &(instance->rx_pkt);
+        return &(instance->rxpkt);
     else
         return 0;
 }
 
 void _serials_send_byte(serials_t *instance, uint8_t data)
 {
-    instance->fn_send_byte(data);
+	if(instance->fn_send_byte != 0)
+		instance->fn_send_byte(data);
 }
 void _serials_send_data(serials_t *instance, uint8_t *data, uint32_t count)
 {
@@ -103,8 +139,8 @@ void _serials_send_data(serials_t *instance, uint8_t *data, uint32_t count)
     for(i = 0; i < count; i++)
     {
         if(data[i] == SERIALS_BS_BEGIN ||
-                data[i] == SERIALS_BS_END ||
-                data[i] == SERIALS_BS_ESC)
+           data[i] == SERIALS_BS_END ||
+           data[i] == SERIALS_BS_ESC)
         {
             _serials_send_byte(instance, SERIALS_BS_ESC);
         }
@@ -116,38 +152,62 @@ void _serials_pkt_send(serials_t *instance, serials_pkt_t *pkt)
 {
     _serials_send_byte(instance, SERIALS_BS_BEGIN);
     _serials_send_data(instance, (uint8_t*)&(pkt->cmd), 1);
+#if SERIALS_ARG_TO_ASCII
+    char arg_hex[2];
+    if(pkt->arg != SERIALS_ARG_NODATA && pkt->data_count == 0)
+    {
+        _ui8tohex(pkt->arg, arg_hex);
+        pkt->arg = SERIALS_ARG_DATA;
+        pkt->data = (uint8_t*) arg_hex;
+        pkt->data_count = 2;
+    }
+#endif
     _serials_send_data(instance, &(pkt->arg), 1);
+
     if(pkt->data != 0 && pkt->data_count > 0)
         _serials_send_data(instance, pkt->data, pkt->data_count);
     _serials_send_byte(instance, SERIALS_BS_END);
+#if SERIALS_APPEND_CRLF
+    _serials_send_byte(instance, (uint8_t) '\r');
+    _serials_send_byte(instance, (uint8_t) '\n');
+#endif
 }
 
 void _serials_pkt_init(serials_pkt_t *pkt)
 {
-    pkt->arg = SERIALS_NO_ARG;
+	pkt->cmd = SERIALS_CMD_NULL;
+    pkt->arg = SERIALS_ARG_NODATA;
     pkt->data = 0;
     pkt->data_count = 0;
 }
 
 void _serials_rxpkt_build(serials_t *instance)
 {
-    uint8_t *rxdata = instance->rx_buf;
+    uint8_t *rxdata = instance->rxbuf;
     uint32_t rxdata_count = instance->rx_cnt;
-    serials_pkt_t *pkt = &(instance->rx_pkt);
-    pkt->cmd = (serials_cmd_t) rxdata[0];
-    pkt->arg = rxdata[1];
-    pkt->data_count = rxdata_count - 2;
-    if(pkt->data_count > 0)
+    serials_pkt_t *pkt = &(instance->rxpkt);
+    if(rxdata_count >= 2)
     {
-        pkt->data = rxdata + 2;
+		pkt->cmd = (serials_cmd_t) rxdata[0];
+		pkt->arg = rxdata[1];
+		pkt->data_count = rxdata_count - 2;
+		if(pkt->data_count > 0)
+		{
+			pkt->data = rxdata + 2;
+			if(pkt->cmd == SERIALS_CMD_STRING)
+			{
+				pkt->data[pkt->data_count] = '\0';
+			}
+		}
     }
 }
 
-void serials_send_ack(serials_t *instance)
+void serials_send_ack(serials_t *instance, uint8_t cmd)
 {
     serials_pkt_t pkt;
     _serials_pkt_init(&pkt);
     pkt.cmd = SERIALS_CMD_ACK;
+    pkt.arg = cmd;
     _serials_pkt_send(instance, &pkt);
 }
 
@@ -165,8 +225,20 @@ void serials_send_bytes(serials_t *instance, uint8_t *data, uint32_t count)
     serials_pkt_t pkt;
     _serials_pkt_init(&pkt);
     pkt.cmd = SERIALS_CMD_BYTES;
+    pkt.arg = SERIALS_ARG_DATA;
     pkt.data = data;
     pkt.data_count = count;
     _serials_pkt_send(instance, &pkt);
+}
+
+void serials_send_string(serials_t *instance, char *str)
+{
+	serials_pkt_t pkt;
+	_serials_pkt_init(&pkt);
+	pkt.cmd = SERIALS_CMD_STRING;
+	pkt.arg = SERIALS_ARG_DATA;
+	pkt.data = (uint8_t*) str;
+	pkt.data_count = strlen(str);
+	_serials_pkt_send(instance, &pkt);
 }
 
--- src/serials.h
+++ src/serials.h
@@ -4,24 +4,42 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifndef SERIALS_ECHO_RX
+#define SERIALS_ECHO_RX			0
+#endif
+
+#ifndef SERIALS_APPEND_CRLF
+#define SERIALS_APPEND_CRLF		1
+#endif
+
+#ifndef SERIALS_ARG_TO_ASCII
+#define SERIALS_ARG_TO_ASCII	0
+#endif
+
 // Byte-stuffing bytes
-#define SERIALS_BS_BEGIN	0x0D 	// '\r'
-#define SERIALS_BS_END	    0x0A 	// '\n'
-#define SERIALS_BS_ESC	    0x5E	// '^'
+#define SERIALS_BS_BEGIN	0x3C 	// '<'
+#define SERIALS_BS_END	    0x3E 	// '>'
+#define SERIALS_BS_ESC	    0x2A	// '*'
 
 // Packet structure (without byte stuffing)
 // [CMD] [ARG] [DATA]*
 // The argument belongs to the CMD while DATA is set
 // by the user. All fields are 1-byte wide, except DATA which is a
 // byte array.
+// Examples:
+// <s,Hello World>
+// <s,*<Hello*>>
 
-#define SERIALS_NO_ARG	0xFF
+#define SERIALS_ARG_DATA	0x2C	// ','
+#define SERIALS_ARG_NODATA	0x00	// null
 
 typedef enum serials_cmd_e
 {
-	SERIALS_CMD_ACK 		= 0xAA,
-	SERIALS_CMD_ERR 		= 0xFF,
-	SERIALS_CMD_BYTES	= 0x01,
+	SERIALS_CMD_NULL		= 0x00,
+	SERIALS_CMD_ACK 		= 0x6b,	// 'k'
+	SERIALS_CMD_ERR 		= 0x65,	// 'e'
+	SERIALS_CMD_BYTES		= 0x62, // 'b'
+	SERIALS_CMD_STRING		= 0x73, // 's'
 } serials_cmd_t;
 
 typedef struct serials_pkt_s
@@ -34,35 +52,45 @@ typedef struct serials_pkt_s
 
 typedef struct serials_init_s
 {
-	uint8_t *rx_buf;
-	uint32_t rx_buf_len;
+	uint8_t *rxbuf;
+	uint32_t rxbuf_len;
 	void (*fn_send_byte)(uint8_t data);
 	void (*cb_rxpkt_ready)(void);
 } serials_init_t;
 
 typedef struct serials_s
 {
-	serials_pkt_t rx_pkt;
-	uint8_t *rx_buf;
-	uint32_t rx_buf_len;
+	serials_pkt_t rxpkt;
+	uint8_t *rxbuf;
+	uint32_t rxbuf_len;
 	uint32_t rx_cnt;
 	bool bs_esc;
 	bool overflow;
 	bool rxpkt_ready;
+	bool rxing;
 	void (*fn_send_byte)(uint8_t data);
 	void (*cb_rxpkt_ready)(void);
 } serials_t;
 
+typedef enum serials_err_e
+{
+	SERIALS_ERR_INVALID_CMD 	= 0x31,
+	SERIALS_ERR_INVALID_LEN 	= 0x32,
+	SERIALS_ERR_INVALID_DATA	= 0x33,
+} serials_err_t;
+
 
 void serials_init(serials_t *instance, serials_init_t *init);
+void serials_reset(serials_t *instance);
 void serials_parse_bytes(serials_t *instance, uint8_t *data, uint32_t count);
 bool serials_overflow(serials_t *instance);
 bool serials_rxpkt_ready(serials_t *instance);
 void serials_rxpkt_ready_clear(serials_t *instance);
 serials_pkt_t* serials_rxpkt(serials_t *instance);
 
-void serials_send_ack(serials_t *instance);
+void serials_send_ack(serials_t *instance, uint8_t cmd);
 void serials_send_err(serials_t *instance, uint8_t err_code);
 void serials_send_bytes(serials_t *instance, uint8_t *data, uint32_t count);
+void serials_send_string(serials_t *instance, char *str);
 
 #endif // SERIALS_H
